d4351f46541841bbed9e64da939611be0e1bf79b
thanhmin34 <79617357+thanhmin34@users.noreply.github.com>
2022-06-10 08:38:57 +0700

    setup otp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useCallback, useEffect, useState } from "react";
+import InputMask from "react-input-mask";
+function PhoneInput(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <InputMask
+      mask="999-999-9999-99999999"
+      value={props.value}
+      type="tel"
+      maskChar={null}
+      className="border border-[#e5e5e5] px-6 py-2 outline-none focus:border-slate-700 "
+      onChange={props.onChange}
+      placeholder="500-000-000"
+      // alwaysShowMask={true}
+    ></InputMask>
+  );
+}
+function App() {
+  const [phone, setPhone] = useState("");
+  const number = "35";
+  useEffect(() => {
+    let result = +phone.replaceAll("-", "");
+    console.log(+(number + result));
+  }, [phone]);
+  const handleInput = ({ target: { value } }) => setPhone(value);
+  return (
+    <div className="flex items-center justify-center mt-16 flex-col">
+      <PhoneInput value={phone} onChange={handleInput}></PhoneInput>
+      <div style={{ paddingTop: "12px" }}>Phone: {phone}</div>
     </div>
   );
 }
-
 export default App;
